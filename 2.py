36727a5c157a7395d4d1458117f4f425b70a5f1c
Zex-code <Sasha20000019@gmail.com>
2025-03-27 13:16:55 +0300

    Update LR2 2.py

--- 2.py
+++ 2.py
@@ -1,81 +1,48 @@
-import pygame
-from pygame.draw import *
+from turtle import *
+import turtle
 
-pygame.init()
+speed(0)
+shape('turtle')
 
+num3 = [(50, 0), (0,-50), (50,-50), (0,-100)]
+num9 = [(50,0), (50,-50,), (0,-50), (0,0), (0,-50), (50,-50), (0,-100)]
+num4 = [(0,-50), (50,-50), (50,0), (50,-50), (50,-100)]
+num0 = [(0,-100), (50,-100), (50,0), (0,0)]
 
-def layer_1():
-    mntn = [[0, 330]]
-    for i in range(10, 240):
-        a = 280-(i-10)**2/400
-        mntn.append([i, a])
-    mntn.append([290, 130])
-    mntn.append([310, 170])
-    mntn.append([430, 250])
-    mntn.append([500, 240])
-    mntn.append([540, 270])
-    mntn.append([590, 200])
-    mntn.append([650, 220])
-    mntn.append([680, 190])
-    for i in range(680, 800):
-        a = 190 + (i-680)*(i-700)*(i-830)/4000
-        mntn.append([i, a])
-    mntn.append([860, 170])
-    mntn.append([900, 160])
-    for i in range(900, 970):
-        a = 160 + (i-900)**2/200
-        mntn.append([i, a])
-    mntn.append([1000, 180])
-    mntn.append([1050, 213])
+for i in num3:
+    goto(i[0], i[1])
+penup()
+goto(60, 0)
+pendown()
 
-    polygon(screen, (252, 152, 49), mntn)
+for i in num9:
+    goto(i[0]+60, i[1])
+penup()
+goto(120, 0)
+pendown()
 
+for i in num4:
+    goto(i[0]+120, i[1])
+penup()
+goto(180, 0)
+pendown()
 
-def layer_2():
-    mntn = [[0, 470], [0, 350], [10, 350], [35, 370]]
-    for i in range(35, 230):
-        a = 370 + (i-35)*(i-200)/70
-        mntn.append([i, a])
-    mntn.append([280, 370])
-    mntn.append([350, 420])
-    mntn.append([390, 320])
-    mntn.append([490, 350])
-    mntn.append([560, 420])
-    mntn.append([650, 400])
-    for i in range(650, 900):
-        a = 400 + 450*((650/i)**12 - (650/i)**6)
-        mntn.append([i, a])
-    mntn.append([950, 280])
-    mntn.append([1020, 295])
-    mntn.append([1050, 230])
-    mntn.append([1050, 450])
+for i in num0:
+    goto(i[0]+180, i[1])
+penup()
+goto(240, 0)
+pendown()
 
-    polygon(screen, (172, 67, 52), mntn)
+for i in num9:
+    goto(i[0]+240, i[1])
+penup()
+goto(300, 0)
+pendown()
 
+for i in num3:
+    goto(i[0]+300, i[1])
+penup()
+goto(360, 0)
+pendown()
 
-FPS = 30
-screen = pygame.display.set_mode((1050, 700))
-screen.fill((255, 207, 171))
-
-# background
-rect(screen, (254, 213, 196), (0, 150, 1050, 150))
-rect(screen, (179, 134, 148), (0, 450, 1050, 250))
-
-# sun
-circle(screen, (252, 238, 33), (525, 140), 70)
-
-# mountains
-layer_1()
-layer_2()
-
-pygame.display.update()
-clock = pygame.time.Clock()
-finished = False
-
-while not finished:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-
-pygame.quit()
+turtle.mainloop()
