6f4c41017e2415fc7effdbf3429765eafe28af95
Zex-code <Sasha20000019@gmail.com>
2025-03-27 13:15:29 +0300

    Update LR 2 1.py

--- 1.py
+++ 1.py
@@ -1,38 +1,12 @@
-import pygame
-from pygame.draw import *
+import turtle
+from random import *
 
-pygame.init()
+turtle.shape('classic')
+turtle.speed(0)
 
-FPS = 30
-screen = pygame.display.set_mode((400, 400))
+for x in range(1000):
+    a = randint(-360, 360)
+    turtle.right(a)
+    turtle.forward(10)
 
-screen.fill((220, 220, 220))
-
-circle(screen, (255, 255, 0), (200, 200), 100)
-circle(screen, (0, 0, 0), (200, 200), 101,2)
-
-circle(screen, (255, 0, 0), (150, 175), 20)
-circle(screen, (0, 0, 0), (150, 175), 21, 2)
-
-circle(screen, (255, 0, 0), (250, 175), 20)
-circle(screen, (0, 0, 0), (250, 175), 21, 2)
-
-circle(screen, (0, 0, 0), (150, 175), 5)
-circle(screen, (0, 0, 0), (250, 175), 5)
-
-polygon(screen, (0, 0, 0), [(70, 50), (85, 35), (185, 135), (170, 150)])
-polygon(screen, (0, 0, 0), [(210, 135), (225, 150), (365, 75), (350, 60)])
-
-rect(screen, (0, 0, 0), (125, 220, 150, 30))
-
-pygame.display.update()
-clock = pygame.time.Clock()
-finished = False
-
-while not finished:
-    clock.tick(FPS)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            finished = True
-
-pygame.quit()
+turtle.mainloop()
